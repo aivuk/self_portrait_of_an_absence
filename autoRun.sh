da376d471b6d039f6766cfee5d77e49da978405e
Radamés Ajna <radamajna@gmail.com>
2016-09-19 16:27:06 +0200

    better run script

--- autoRun.sh
+++ autoRun.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #must be on ~/ .baschrc
 #using standalone version of Supercolider
+SCDIR = /home/pi/supercolliderStandaloneRPI2 #sc dir
+SCFILE = /home/pi/self_portrait_of_an_absence/scscripts/graziele_firstTestsEyetrackingSounds.scd
+PYTHONF = /home/pi/self_portrait_of_an_absence/python/opt_flow_PI.py
 export DISPLAY=:0.0
 export `dbus-launch | grep ADDRESS`
 export `dbus-launch | grep PID`
 jackd -P95 -dalsa -dhw:0 -p1024 -n3 -s -r44100 &
-cd supercolliderStandaloneRPI2 #needs SC standalone version
-python /home/pi/self_portrait_of_an_absence/python/opt_flow_PI.py 0 &
-xvfb-run --auto-servernum ./sclang -a -l sclang.yaml /home/pi/self_portrait_of_an_absence/scscripts/graziele_firstTestsEyetrackingSounds.scd #main supercollider script
+python PYTHONF 0 &
+xvfb-run --auto-servernum SCDIR/sclang -a -l SCDIR/sclang.yaml SCFILE #main supercollider script
