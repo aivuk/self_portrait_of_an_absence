2f624c68fff7fb8cc9d21eca3af1ba076f1cbdcb
Radamés Ajna <radamajna@gmail.com>
2016-09-19 16:57:48 +0200

    back to original solution

--- autoRun.sh
+++ autoRun.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #must be on ~/ .baschrc
 #using standalone version of Supercolider
-SCDIR=/home/pi/supercolliderStandaloneRPI2 #sc dir
 SCFILE=/home/pi/self_portrait_of_an_absence/scscripts/graziele_firstTestsEyetrackingSounds.scd
 PYTHONF=/home/pi/self_portrait_of_an_absence/python/opt_flow_PI.py
 export DISPLAY=:0.0
@@ -9,4 +8,5 @@ export `dbus-launch | grep ADDRESS`
 export `dbus-launch | grep PID`
 jackd -P95 -dalsa -dhw:0 -p1024 -n3 -s -r44100 &
 python $PYTHONF 0 &
-xvfb-run --auto-servernum $SCDIR/sclang -a -l $SCDIR/sclang.yaml $SCFILE #main supercollider script
+cd /home/pi/supercolliderStandaloneRPI2
+xvfb-run --auto-servernum ./sclang -a -l sclang.yaml $SCFILE #main supercollider script
