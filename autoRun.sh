680278df13393f3cb9b79a00b4fc8e1abc1e583a
Radamés Ajna <radamajna@gmail.com>
2016-09-19 16:13:47 +0200

    autorun for python and supercollider relative to the repo

--- autoRun.sh
+++ autoRun.sh
@@ -6,4 +6,5 @@ export `dbus-launch | grep ADDRESS`
 export `dbus-launch | grep PID`
 jackd -P95 -dalsa -dhw:0 -p1024 -n3 -s -r44100 &
 cd supercolliderStandaloneRPI2 #needs SC standalone version
-xvfb-run --auto-servernum ./sclang -a -l sclang.yaml main_script.scd #main supercollider script
+python /home/pi/self_portrait_of_an_absence/python/opt_flow_PI.py 0 &
+xvfb-run --auto-servernum ./sclang -a -l sclang.yaml /home/pi/self_portrait_of_an_absence/scscripts/graziele_firstTestsEyetrackingSounds.scd #main supercollider script
